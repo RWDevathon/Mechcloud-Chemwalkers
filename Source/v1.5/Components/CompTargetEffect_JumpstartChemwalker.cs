db636f4ce351a7810a0f37f845abc8672c151ba1
Killathon <huelva21@gmail.com>
2025-04-10 20:44:49 -0400

    Capability to jumpstart chemwalker awakening

---
+++ Source/v1.5/Components/CompTargetEffect_JumpstartChemwalker.cs
@@ -0,0 +1,38 @@
+﻿using RimWorld;
+using Verse.AI;
+using Verse;
+
+namespace ArtificialBeings
+{
+    // Target controller for jumpstarting a chemwalker.
+    public class CompTargetEffect_JumpstartChemwalker : CompTargetEffect
+    {
+        public CompTargetEffectProperties_JumpstartChemwalker Props
+        {
+            get
+            {
+                return (CompTargetEffectProperties_JumpstartChemwalker)props;
+            }
+        }
+
+        public override bool CanApplyOn(Thing target)
+        {
+            return target is ABF_ArmorPawnSpawner spawner && spawner.Active && spawner.workRemaining > 0;
+        }
+
+        public override void DoEffectOn(Pawn user, Thing target)
+        {
+            if (!user.CanReserveAndReach(target, PathEndMode.Touch, Danger.Deadly))
+            {
+                Messages.Message("CannotReach".Translate(), MessageTypeDefOf.RejectInput, false);
+            }
+
+            if (target is ABF_ArmorPawnSpawner spawner && spawner.workRemaining > 0)
+            {
+                Job job = JobMaker.MakeJob(ABF_JobDefOf.ABF_Job_Chemwalker_JumpstartChemwalker, target, parent);
+                job.count = 1;
+                user.jobs.TryTakeOrderedJob(job, JobTag.Misc);
+            }
+        }
+    }
+}
---
+++ Source/v1.5/JobDrivers/JobDriver_JumpstartChemwalker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace ArtificialBeings
+{
+    public class JobDriver_JumpstartChemwalker : JobDriver
+    {
+        private ABF_ArmorPawnSpawner Armor => (ABF_ArmorPawnSpawner)job.GetTarget(TargetIndex.A).Thing;
+
+        private Thing Item => job.GetTarget(TargetIndex.B).Thing;
+
+        private Mote warmupMote;
+
+        public override bool TryMakePreToilReservations(bool errorOnFailed)
+        {
+            if (pawn.Reserve(Armor, job, 1, -1, null, errorOnFailed))
+            {
+                return pawn.Reserve(Item, job, 1, -1, null, errorOnFailed);
+            }
+            return false;
+        }
+
+        protected override IEnumerable<Toil> MakeNewToils()
+        {
+            yield return Toils_Goto.GotoThing(TargetIndex.B, PathEndMode.Touch).FailOnDespawnedOrNull(TargetIndex.B).FailOnDespawnedOrNull(TargetIndex.A);
+            yield return Toils_Haul.StartCarryThing(TargetIndex.B);
+            yield return Toils_Goto.GotoThing(TargetIndex.A, PathEndMode.Touch).FailOnDespawnedOrNull(TargetIndex.A);
+            Toil toil = Toils_General.Wait(600);
+            toil.WithProgressBarToilDelay(TargetIndex.A);
+            toil.FailOnDespawnedOrNull(TargetIndex.A);
+            toil.FailOnCannotTouch(TargetIndex.A, PathEndMode.Touch);
+            toil.tickAction = delegate
+            {
+                CompUsable compUsable = Item.TryGetComp<CompUsable>();
+                if (compUsable != null && warmupMote == null && compUsable.Props.warmupMote != null)
+                {
+                    warmupMote = MoteMaker.MakeAttachedOverlay(Armor, compUsable.Props.warmupMote, Vector3.zero);
+                }
+
+                warmupMote?.Maintain();
+            };
+            yield return toil;
+            yield return Toils_General.Do(new Action(Jumpstart));
+        }
+
+        // Reduce the target shell's work remaining by the designated amount of ticks from the target effect properties.
+        private void Jumpstart()
+        {
+            int jumpstartTicks = Item.def.GetCompProperties<CompTargetEffectProperties_JumpstartChemwalker>().jumpstartTicks;
+            Armor.workRemaining = Math.Max(0, Armor.workRemaining - jumpstartTicks);
+            Item.SplitOff(1).Destroy(DestroyMode.Vanish);
+        }
+    }
+}
