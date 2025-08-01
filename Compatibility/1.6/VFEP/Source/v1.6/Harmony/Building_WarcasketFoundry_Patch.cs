422ef222e1c84e987292d1fd09afb4c593e0ee78
Killathon <huelva21@gmail.com>
2025-08-01 13:21:28 -0400

    Updated compatibility

--- Compatibility/1.6/VFEP/Source/v1.6/Harmony/Building_WarcasketFoundry_Patch.cs
+++ Compatibility/1.6/VFEP/Source/v1.6/Harmony/Building_WarcasketFoundry_Patch.cs
@@ -6,14 +6,14 @@ namespace ArtificialBeings
 {
     public class Building_WarcasketFoundry_Patch
     {
-        // Mechclouds cannot be entombed into warcaskets. Use the DeathActionWorker as there exists no other identification method at the moment.
+        // Mechclouds cannot be entombed into warcaskets.
         [HarmonyPatch(typeof(Building_WarcasketFoundry), nameof(Building_WarcasketFoundry.CannotUseNowReason))]
         public class Building_WarcasketFoundry_CannotUseNowReason_Patch
         {
             [HarmonyPostfix]
             public static void Listener(ref string __result, Pawn selPawn)
             {
-                if (__result == null && selPawn.RaceProps.DeathActionWorker is DeathActionWorker_Mechcloud)
+                if (__result == null && selPawn.def.race.FleshType == ABF_FleshTypeDefOf.ABF_FleshType_Chemwalker_Base)
                 {
                     __result = "ABF_MechcloudsCannotUseThis".Translate();
                 }
